dfb2cc002edb5cd03aa5ae5e9577822e88563403
EC2 Default User <ec2-user@ip-172-31-5-36.ap-northeast-1.compute.internal>
2023-08-10 11:12:23 +0000

    [Add]ユーザー認証機能、投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :books, only: [:new, :index, :show]
+
   devise_for :users
+  get '/' => 'homes#top', as: 'top'
+  get 'home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
